e6d7017927daa9ec4c68a1679a9dacb4eeda458f
Mahir Jain <mahirjain2006@gmail.com>
2024-06-20 17:19:54 +0530

    Add files via upload

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,1 +1,19 @@
-
+export const metadata = {
+    title: 'Login',
+    description: 'Login to FTP Portal',
+  }
+  
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>
+          {children}
+        </body>
+      </html>
+    )
+  }
+  
