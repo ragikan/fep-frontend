8dee742d4baaca318d7890acb7967c7baf744a89
IceShuttle <IceShuttle@linux>
2024-06-28 06:00:22 +0530

    tailed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-/* import "./globals.css"; */
+import "./globals.css";
 import Navbar from "@/components/Navbar";
 
 
--- app/professor/communications/page.tsx
+++ app/professor/communications/page.tsx
@@ -6,16 +6,17 @@ import { useState } from 'react';
 const Project = () =>{
   const [title,setTitle]= useState("Very Important Project");
   const [desc,setDesc]= useState("A Very Important Project");
-  return <li>
-    <h3>{title}</h3>
+  return <li className="flex justify-between gap-x-6 py-5 bg-gray-100 shadow-md rounded px-9">
+    <h3 className="text-sm font-semibold leading-6 text-gray-900">{title}</h3>
     <p>{desc}</p>
-    <button>Chat now!</button>
+    <button className="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded mt-10 sm:mt-0" >Chat now!</button>
   </li>
 }
 
 const Communications = () => {
-  return <>Project Communications
-  <ul>
+  return <>
+  <h1 className="md:text-3xl text-2xl font-medium title-font text-gray-900 text-center">PROJECT COMMUNICATIONS</h1>
+  <ul role="list" className="divide-y divide-gray-100 border space-y-2">
     <Project />
     <Project />
     <Project />
