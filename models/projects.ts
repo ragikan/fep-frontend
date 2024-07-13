06b624bdf5c2f7c0efd60cd4641c27e162904212
tusharj23 <154343368+tusharj23@users.noreply.github.com>
2024-07-13 17:43:42 +0530

    Update projects.ts

--- models/projects.ts
+++ models/projects.ts
@@ -1,1 +1,17 @@
-
+// models/project.ts
+export interface Project {
+    ID: string;
+    project_name: string;
+    image: string;
+    university_name: string;
+    project_details: string;
+    universityDescription: string;
+    project_mode: string;
+    application_deadline: string;
+    stipend: string;
+    eligibility: string;
+    university_location: string;
+    ranking: string;
+    coordinates: [number, number];
+  }
+  
