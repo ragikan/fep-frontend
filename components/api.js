a41fecc0db5fece8b2c248e54ba437aa560fa7f0
tusharj23 <154343368+tusharj23@users.noreply.github.com>
2024-07-13 17:46:03 +0530

    Update api.js

--- components/api.js
+++ components/api.js
@@ -1,8 +1,8 @@
-// components/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3492/api/admin/project'; 
+const API_BASE_URL = 'http://localhost:3492/api/admin/project';
 
 // Project APIs
-export const getAllProjects = () => axios.get(`${API_BASE_URL}/admin/project`);
-export const getProjectById = (id) => axios.get(`${API_BASE_URL}/admin/project/${id}`);
+export const getAllProjects = () => axios.get(`${API_BASE_URL}`);
+export const getProjectById = (id) => axios.get(`${API_BASE_URL}/${id}`);
+export const createProject = (project) => axios.post(`${API_BASE_URL}`, project);
